7a414cd4da519fe7db3c9037db8cf0816e0cfff1
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-04 18:20:35 +0530

    [sarthak]ADD:Switchcase

---
+++ Switchcase/UC4_SWITCHCASE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC4_SWITCHCASE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_PART_TIME = 1;
+            const  int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            
+            int empHrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            switch (empcheck)
+            {
+                    case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                    case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empwage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine(" Emp Wage:" + empwage);
+            Console.ReadKey();
+        }
+    }
+}
