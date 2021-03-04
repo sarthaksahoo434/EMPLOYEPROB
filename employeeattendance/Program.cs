c8f2e274d1b1258827de9a3c0863ec6c46f6cbc6
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-04 17:51:20 +0530

    [sarthak]ADD:EMPLOYEEPROB

---
+++ employeeattendance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace employeeattendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME =0;
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+                Console.ReadKey();
+            
+        }
+    }
+}
